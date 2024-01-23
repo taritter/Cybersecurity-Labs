725d35c99dd366748172c5acee1519ebaeaddd60
Therese Ritter <98991018+taritter@users.noreply.github.com>
2024-01-23 12:10:33 -0500

    added documentation

--- Lab1.0/Lab1.0Therese.py
+++ Lab1.0/Lab1.0Therese.py
@@ -8,25 +8,33 @@ This is a program to log in to a spy companies intranet system
 
 import csv
 
-
+#opens csv file
 file = open('Spy_Data.csv')
 
+#differnt levels differnt users can access
 spy = [1,4,5,6]
 tech = [1,2,4,5]
 admin = [1,2,3,4,5,6]
 
-
+#logging in
 user = input("Enter your username: ")
 user_pw = input("Enter your password: ")
+
 
 accepted = False
 valid = False
 
+#goes through each line in csv
 for line in file:
+    #splits line by ","
     info = line.split(",")
+    #if the line split matches the username and password entered, accepted is true
     if user == info[0] and user_pw == info[1]:
         accepted = True
+        #changes level to int
         level = int(info[2].strip('\n'))
+
+        #sets the employee/user type
         if level == 1:
             employee = spy
         elif level == 2:
@@ -34,16 +42,22 @@ for line in file:
         else:
             employee = admin
         print("Welcome " + user + ", you now have access to SpyHub")
+        
         while not valid:
+            #if non int is inputted, shows menu again
             try:
                 menu = int(input("Choose a number selection from the menu:\n1. Profile\n2. Police Data\n3. Admin\n4. Weapons\n5. Blueprints/Maps\n6. Assignments\n\n"))
+                #if not one of the menu options
                 if menu not in employee and menu not in range(1,6):
                     print("Not a valid option.")
+                #if is menu option, but employee doesn't have authorization
                 elif menu not in employee and menu in range(1,6):
                     print("You are not authorized to access this area.\n")
                     back = input("Type 'r' to go back, type 'q' to leave\n")
+                    #quits progrm
                     if back == 'q':
                         valid = True
+                #if employee does have authorization
                 elif menu in employee:
                     valid = True
                     if menu == 1:
@@ -58,9 +72,10 @@ for line in file:
                         print("You now have access to blueprints/maps")
                     else:
                         print("You now have access to previous and current assignments")
+            #if non int
             except ValueError:
                 print("Not a valid option.")
-
+#if not a user
 if accepted == False:
     print("Invalid user")
 
